64032ddd5cd83ac0fb25c7acf1856a519ba00a53
juanpunal <punaljuan@gmail.com>
2021-10-16 17:35:46 -0300

    arreglos

--- Javascript/Clase7.js
+++ Javascript/Clase7.js
@@ -1,42 +1,111 @@
-let elemento = document.getElementById("principal");
-let bool = false;
-let vv = "buen dia";
-
-elemento.innerHTML = "bool";
-
-var r =Number(" ");
-
-console.log(r);
 
 
-elemento.innerHTML = "<p>Prueba 123</p>"
+let elmento = document.querySelector("#principal")
 
-if(bool){
-    document.write("<h1> prueba * 2 </h1 >")
+let mascotas = [];
+
+;
+
+
+const agregarMascota = () =>{
+    let datos = document.querySelectorAll("input");
+
+    let consulta = "Urgencia";
+
+    if(datos[7].cheked){
+        consulta = "Consulta"
+    };
+
+    let mascota = {
+        propietario: datos[0].value,
+        Nombre: datos[1].value,
+        Raza: datos[2].value,
+        Escpecie: datos[3].value,
+        Edad: datos[4].value,
+        peso: datos[5].value,
+        Imagen: datos[6].value,
+        tipoConsulta: consulta
+    }
+    mascotas.push(mascota);
+
+    console.log(mascotas)
+    mostrarMascotas()
 }
 
-/* hfijerifje */
+const crearForm=() =>{
 
-/*let mensaje = prompt("Introduce tu mensaje");
-elemento.innerHTML += "<h3>"+ mensaje + "</h3>";
+    elmento.innerHTML = `
+    <form class="col-6">
+        <div class="mb-3">
+            <label for="Propietario" class="form-label">Propietario</label>
+            <input type="text" class="form-control" id="Propietario" >
+        </div>
+        <div class="mb-3">
+            <label for="Nombre" class="form-label">Nombre</label>
+            <input type="text" class="form-control" id="Nombre" >
+        </div>
+        <div class="mb-3">
+            <label for="Raza" class="form-label">Raza</label>
+            <input type="text" class="form-control" id="Raza" >
+        </div>
+        <div class="mb-3">
+            <label for="Especie" class="form-label">Especie</label>
+            <input type="text" class="form-control" id="Especie" >
+        </div>
+        <div class="mb-3">
+            <label for="Edad" class="form-label">Edad</label>
+            <input type="text" class="form-control" id="Edad" >
+        </div>
+        <div class="mb-3">
+            <label for="Peso" class="form-label">Peso</label>
+            <input type="text" class="form-control" id="Peso" >
+        </div>
+        <div class="mb-3">
+            <label for="Imagen" class="form-label">Imagen</label>
+            <input type="text" class="form-control" id="Imagen" >
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+            <label class="form-check-label" for="flexRadioDefault1">
+                Consulta
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio"  name="flexRadioDefault" id="flexRadioDefault2" checked>
+            <label class="form-check-label" for="flexRadioDefault2">
+                Emergencia
+            </label>
+      </div>
 
-let menu = prompt("Selecciona el plato: \n 1. Milanesa de seitan \n 2. Papas fritas");
+        <button type="submit" id="btn" class="btn btn-primary">Agrgar Datos</button>
+    </form>
+    `;
 
-if(menu == 1)
-{
-    elemento.innerHTML = "<h2> Elegiste seitan </h2>"
-}
-else{
-    elemento.innerHTML = "<h2> Elegiste papas </h2>"
-}*/
+     document.getElementById('btn').addEventListener('click', agregarMascota);
 
 
-let num = parseInt(prompt("Ingrsar el primer numero: "))
+};
 
-let num2 = parseInt(prompt("Ingrsar el numero: "))
+crearForm()
 
-let num3 = (num + num2);
+const mostrarMascotas= ()=>{
+    mascotas.forEach(m =>{
+        elmento.innerHTML =""
+        elmento.innerHTML += `
+        <div class="card" style="width: 18rem;">
+            <img src="${m.Imagen}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${m.Nombre}</h5>
+                <p class="card-text">Especie: ${m.Escpecie}</p>
+                <p class="card-text">Raza: ${m.Raza}</p>
+                <p class="card-text">Edad: ${m.Edad}</p>
+                <p class="card-text">Peso: ${m.peso}</p>
+                <a href="#" class="btn btn-primary" onCLick=crearForm()>Go somewhere</a>
+            </div>
+        </div>
+        `
 
-elemento.innerHTML = "<h2> El resutlado de la suma es " + num3 + "</h2>"
 
-console.log(typeof(num))
+    })
+};
+
