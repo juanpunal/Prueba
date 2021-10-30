d22ea011abbab71f48f2b408b777d95b687784ff
juanpunal <punaljuan@gmail.com>
2021-10-30 17:14:31 -0300

    Sql

---
+++ NewFolder/mi_primer_aplicaion/src/mi_primer_aplicaion/Mi_primer_aplicaion.java
@@ -0,0 +1,20 @@
+
+package mi_primer_aplicaion;
+
+public class Mi_primer_aplicaion {
+    
+    
+    
+    
+    public static void main(String[] args) {
+        int numeros;
+        String string;
+        float porcentajes;
+        boolean bool;
+        numeros = 10;
+        float resultado;
+        resultado = numeros + 17;
+        System.out.println(resultado );
+    }
+    
+}
